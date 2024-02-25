3e3d9ac919b6579b632596f571f70b988d1bd213
dharmik48 <hingudharmik4@gmail.com>
2024-02-25 20:35:34 +0530

    feat: adds main loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import MainLoading from '@/components/main-loading'
+import { AnimatePresence } from 'framer-motion'
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<div className='z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex'></div>
+		<main className='flex min-h-screen relative flex-col items-center justify-center overflow-hidden'>
+			<MainLoading key={'loader'} />
 		</main>
 	)
 }
---
+++ src/components/main-loading.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { useState } from 'react'
+import { AnimatePresence, Variants, motion } from 'framer-motion'
+
+const DURATION = 1
+
+const MainLoading = () => {
+	const [animationState, setAnimationState] = useState('live')
+
+	const colors = [
+		{ bg: '#331d45', text: '#a35ddd' },
+		{ bg: '#48173c', text: '#e04db7' },
+		{ bg: '#591b1c', text: '#ee5255' },
+		{ bg: '#58341a', text: '#ed9351' },
+		{ bg: '#6e571e', text: '#ffc944' },
+		{ bg: '#6e6c25', text: '#fffa55' },
+		{ bg: '#244625', text: '#54d457' },
+		{ bg: '#193533', text: '#61cdc5' },
+		{ bg: '#1c2f4d', text: '#528be5' },
+	]
+	const variants = {
+		text: { color: colors.map(c => c.text) },
+		bg: { backgroundColor: colors.map(c => c.bg) },
+		hide: { translateX: '100%' },
+	}
+
+	if (animationState === 'hidden') return null
+
+	return (
+		<AnimatePresence>
+			<motion.div
+				className='absolute inset-0 items-center justify-center flex'
+				initial={{ backgroundColor: colors[0].bg }}
+				animate={animationState === 'done' ? 'hide' : 'bg'}
+				variants={variants}
+				transition={{
+					duration: animationState === 'done' ? DURATION / 2 : DURATION,
+					type: animationState === 'done' ? 'spring' : 'keyframes',
+				}}
+				onAnimationComplete={e =>
+					e === 'bg' ? setAnimationState('done') : setAnimationState('hidden')
+				}
+			>
+				<div className='flex flex-col items-center justify-between'>
+					<motion.h1
+						className='font-black drop-shadow-sm text-9xl'
+						initial={{
+							color: colors[0].text,
+						}}
+						animate={'text'}
+						variants={variants}
+						transition={{ duration: DURATION, type: 'keyframes' }}
+					>
+						iro!
+					</motion.h1>
+					<ColorAnimatedSpan
+						text='Can you find the matching color?'
+						variants={variants}
+						colors={colors}
+						className='text-lg tracking-widest'
+					/>
+				</div>
+				<ColorAnimatedSpan
+					text='Loading...'
+					variants={variants}
+					colors={colors}
+					className='absolute bottom-10 right-12 text-lg'
+				/>
+			</motion.div>
+		</AnimatePresence>
+	)
+}
+
+export default MainLoading
+
+const ColorAnimatedSpan = ({
+	text,
+	colors,
+	variants,
+	className,
+}: {
+	text: string
+	colors: { text: string; bg: string }[]
+	variants: Variants
+	className?: string
+}) => {
+	return (
+		<motion.span
+			className={className}
+			initial={{ color: colors[0].text }}
+			animate={'text'}
+			variants={variants}
+			transition={{ duration: DURATION, type: 'keyframes' }}
+		>
+			{text}
+		</motion.span>
+	)
+}
